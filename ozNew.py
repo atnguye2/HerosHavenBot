fac53e9812fa595563e28d3e265117f611d60239
Brad <bradley.maffett@gmail.com>
2019-07-17 21:20:31 -0400

    The bot will now grant permissions based on emoji reactions to the defined message.

--- ozNew.py
+++ ozNew.py
@@ -1,10 +1,22 @@
 import discord
+import asyncio
 from discord.ext import commands
 from googleSheets import getWelcomeText
 
+#PRODUCTION SERVER
+CHANNELID = '601220239698427915'
+ROLEMESSAGEID = '601220264381906945'
+
+# general (test server) channel id
+# #382727824001335297
+#TESTSERVER
+#CHANNELID = '382727824001335297'
+#ROLEMESSAGEID = '600872091075477521'
+
+#mid = '600884478126063637'
+
 # This is the token found under the https://discordapp.com/developers/applications/509871894061907970/bots
 # It it currently tied to Brad's Discord account
-
 # Change this for production or debug...
 #TESTINGTOKEN.txt is tied to "oz testing" a bot tied to Brad's discord acct.
 #DEBUGTOKEN.txt is currently tied to Oz on the HH server
@@ -19,27 +31,94 @@ gamePresence = discord.Game(name="+help")
 client = commands.Bot(command_prefix='+', pass_context=True, description='')
 startup_extensions = ['ungroupedCommands','dtdCommands','voteCommands', 'characterCommands']
 
+#the client won't listen for reactions on messages not in the client.messages queue. The current max size of this is 5000. To mitigate the chance that reaction message is not in the queue, we will add it every 10 minutes.
+async def append_reaction_message(myChannelID, myMessageID):
+    print("starting append_reaction_message()")
+    myChannel = client.get_channel(myChannelID)
+    myMessage = await client.get_message(myChannel, myMessageID)
+    while True:
+        print(str(len(client.messages)) + " Messages held in client.messages. Adding the Role Reaction Post now.")
+        client.messages.append(myMessage)
+        await asyncio.sleep(3600)
+    print("ending append_reaction_message")
+
+#async def fill_queue(myChannelID, myMessageID):
+#    myChannel = client.get_channel(myChannelID)
+#    myMessage = await client.get_message(myChannel, myMessageID)
+#    for x in range(5005):
+#        print(str(len(client.messages)) + " Messages held in client.messages. WOMP.")
+#        client.messages.append(myMessage)
+
 
 @client.event
 async def on_ready():
+    #await fill_queue(CHANNELID, mid)
     print('We have logged in as {0.user}'.format(client))
     print(discord.__version__)
     await client.change_presence(game=gamePresence, afk=False, status=None)
-
+    await append_reaction_message(CHANNELID, ROLEMESSAGEID)
 
 @client.event
 async def on_message(msg):
     await client.process_commands(msg)
 
-#When a new user joins the server, add the role 'player'
-#Beware that the player role needs to be of lower hierchy than the role assigned to the bot.
+
+
 @client.event
 async def on_member_join(ctx):
     print(ctx.name + ' has joined. Lets make them a player!'.format(client))
     print(getWelcomeText())
-    role = discord.utils.get(ctx.server.roles, name="players")
-    await client.add_roles(ctx, role)
     await client.send_message(ctx, getWelcomeText())
+
+
+
+
+#Beware that the player role needs to be of lower hierchy than the role assigned to the bot.
+@client.event
+async def on_reaction_add(reaction, user):
+    print("a reaction add!")
+    print(reaction.message.id)
+    print(client.messages)
+    if reaction.message.id == ROLEMESSAGEID:
+        print(reaction.emoji)
+        if reaction.emoji == "🎲":
+            print("OK player here 1")
+            role = discord.utils.get(user.server.roles, name="players")
+            await client.add_roles(user, role)
+        if reaction.emoji == "🏰":
+            print("OK DM here")
+            role = discord.utils.get(user.server.roles, name="DMs")
+            await client.add_roles(user, role)
+        if reaction.emoji == "🐲":
+            print("OK PF here")
+            role = discord.utils.get(user.server.roles, name="Pathfinder")
+            await client.add_roles(user, role)
+        else:
+            return
+    else:
+        return
+
+@client.event
+async def on_reaction_remove(reaction, user):
+    print("a reaction remove!")
+    if reaction.message.id == ROLEMESSAGEID:
+        print(reaction.emoji)
+        if reaction.emoji == "🎲":
+            print("attempt to remove player role")
+            role = discord.utils.get(user.server.roles, name="players")
+            await client.remove_roles(user, role)
+        if reaction.emoji == "🏰":
+            print("attempt to remove DM role")
+            role = discord.utils.get(user.server.roles, name="DMs")
+            await client.remove_roles(user, role)
+        if reaction.emoji == "🐲":
+            print("attempt to remove PF role")
+            role = discord.utils.get(user.server.roles, name="Pathfinder")
+            await client.remove_roles(user, role)
+        else:
+            return
+    else:
+        return
 
 @client.command()
 async def load(extension_name: str):
